ef7cc2315834251ff65134782cf47f7e5256659b
Spencer W <4006420+seidior@users.noreply.github.com>
2023-06-03 01:01:56 -0700

    #13 Atomic Design File Structure

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center font-mono text-sm lg:flex">
+        Let&rsquo;s build a health tracker! Get started by editing&nbsp;
+        <code className="font-mono font-bold">app/page.tsx</code>
+      </div>
+    </main>
+  );
+}
