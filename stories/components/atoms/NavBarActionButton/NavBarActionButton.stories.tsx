009d5c25a68899af88fb1ec41b5faa5a9e17327f
Estifanos <estifanosbeyene21@gmail.com>
2023-06-10 17:58:04 -0400

    #27 Add NavBarActionButton component and stories

---
+++ stories/components/atoms/NavBarActionButton/NavBarActionButton.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { NavBarActionButton } from "./NavBarActionButton";
+
+const meta: Meta<typeof NavBarActionButton> = {
+  title: "Atoms/NavBarActionButton",
+  component: NavBarActionButton,
+  parameters: {
+    nextjs: {
+      appDirectory: true
+    }
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof NavBarActionButton>;
+
+export const Primary: Story = {
+  args: {
+    children: "NavBarActionButton",
+    className: "h-20",
+    href: "/",
+    variant: "buttonStyle"
+  }
+};
+
+export const Secondary: Story = {
+  args: {
+    children: "Home",
+    className: "h-20",
+    href: "/",
+    variant: "linkStyle"
+  }
+};
+// <NavBarActionButton variant="linkStyle" href="/login"></NavBarActionButton>
