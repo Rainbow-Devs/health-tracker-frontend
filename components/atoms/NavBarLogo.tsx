98e052a8d440362758c496e23d759e8d128d1a84
Spencer W <4006420+seidior@users.noreply.github.com>
2023-06-13 18:01:57 -0700

    #20 Add logo to Navbar

--- components/atoms/NavBarLogo.tsx
+++ components/atoms/NavBarLogo.tsx
@@ -1,18 +1,40 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+
+import { ColorLogo, MonoLogo } from "@images/WellPathLogo";
 
 export const NavBarLogo: React.FC = () => {
   const router = useRouter();
+  const [wellpathLogo, setWellpathLogo] = useState(<></>);
 
   const clickHandler = () => {
     router.push("/");
   };
 
+  useEffect(() => {
+    const colorSchemeQueryList = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    );
+
+    const setNavbarLogo = (e: MediaQueryList | MediaQueryListEvent) => {
+      console.dir(e);
+      if (e.matches) {
+        setWellpathLogo(<MonoLogo className="w-full" />);
+      } else {
+        setWellpathLogo(<ColorLogo className="w-full" />);
+      }
+    };
+
+    setNavbarLogo(colorSchemeQueryList);
+    colorSchemeQueryList.addEventListener("change", setNavbarLogo);
+  }, []);
+
   return (
     <div className="w-full flex-1 font-bold md:w-auto md:flex-none">
-      <button type="button" onClick={clickHandler}>
-        WellPath
+      <button type="button" onClick={clickHandler} className="w-24 p-1">
+        {wellpathLogo}
       </button>
     </div>
   );
