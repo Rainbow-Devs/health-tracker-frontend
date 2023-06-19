fb9d599c0d90a6ef114c6e3c3f2f04a9b2e03611
Estifanos <estifanosbeyene21@gmail.com>
2023-06-19 14:31:58 -0400

    #20 Add footer design and ImportImage function

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { MarketingPage } from "@templates/MarketingPage";
+import Footer from "@templates/Footer";
 
 export default function Home() {
-  return <MarketingPage />;
+  return (
+    <>
+      <MarketingPage />
+      <Footer />
+    </>
+  );
 }
