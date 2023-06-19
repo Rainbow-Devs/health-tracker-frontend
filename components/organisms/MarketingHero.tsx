c6c1330e42c2c4d4c5bd994346a08a8d9f9f58d5
Spencer W <4006420+seidior@users.noreply.github.com>
2023-06-19 15:59:11 -0700

    #38 WIP on homepage content

--- components/organisms/MarketingHero.tsx
+++ components/organisms/MarketingHero.tsx
@@ -1,11 +1,24 @@
+import { GenericButton } from "@atoms/GenericButton";
+import Link from "next/link";
+
 export const MarketingHero: React.FC = () => {
   return (
-    <section>
+    <section className="flex flex-col space-y-3">
       <header>
-        <h1 className="mb-4 text-lg font-bold">Getting Started</h1>
+        <h2 className="text-lg font-bold">
+          Taking care of yourself is hard. We make it easy.
+        </h2>
       </header>
-      Let&rsquo;s build a health tracker! Get started by editing&nbsp;
-      <code className="font-mono font-bold">app/page.tsx</code>
+      <p>
+        Say goodbye to a million different fitness apps and hello to WellPath.
+        Keep track of your physical and mental health all in one place.
+      </p>
+      <p>
+        Built by <Link href="/about-rainbow-devs">the Rainbow Devs</Link>.
+      </p>
+      <GenericButton href="/login" className="w-24">
+        Sign In
+      </GenericButton>
     </section>
   );
 };
