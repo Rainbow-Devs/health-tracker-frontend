035c80caa3187fba9f4edde39ec38d875e312bba
Paryssatis Khazaie <paryssatis@outlook.com>
2023-06-23 17:21:51 -0700

    #38 Rough version of service section for home page added

--- stories/components/organisms/ServiceList/index.tsx
+++ stories/components/organisms/ServiceList/index.tsx
@@ -5,22 +5,23 @@ export const ServiceList: React.FC = () => {
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
             <div className="mx-auto mb-12 max-w-[510px] text-center lg:mb-20">
-              <span className="mb-2 block text-lg font-semibold text-primary">
-                Our Services
+              <span className="mb-2 block text-lg font-semibold text-white">
+                Our Features
               </span>
-              <h2 className="text-dark mb-4 text-3xl font-bold sm:text-4xl md:text-[40px]">
+              <h2 className="mb-4 text-3xl font-bold text-white sm:text-4xl md:text-[40px]">
                 What We Offer
               </h2>
-              <p className="text-body-color text-base">
-                There are many variations of passages of Lorem Ipsum available
-                but the majority have suffered alteration in some form.
+              <p className="text-base text-white">
+                We make it easier to track all of your health and fitness goals.
+                Currently, you can log your exercises in the fitness log, count
+                calories burned, and set reminders for upcoming workouts.
               </p>
             </div>
           </div>
         </div>
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-            <div className="mb-8 rounded-[20px] bg-white p-10 shadow-md hover:shadow-lg md:px-7 xl:px-10">
+            <div className="mb-8 rounded-[20px] bg-white p-7 shadow-md hover:shadow-lg md:px-7 xl:px-10">
               <div className="mb-8 flex h-[70px] w-[70px] items-center justify-center rounded-2xl bg-primary">
                 <svg
                   width="32"
@@ -35,17 +36,16 @@ export const ServiceList: React.FC = () => {
                   />
                 </svg>
               </div>
-              <h4 className="text-dark mb-3 text-xl font-semibold">
-                Refreshing Design
+              <h4 className="mb-3 text-xl font-semibold text-black">
+                Fitness Log
               </h4>
-              <p className="text-body-color">
-                We dejoy working with discerning clients, people for whom
-                qualuty, service, integrity & aesthetics.
+              <p className="text-black">
+                Log your workouts to highlight progress and note improvements.
               </p>
             </div>
           </div>
           <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-            <div className="mb-8 rounded-[20px] bg-white p-10 shadow-md hover:shadow-lg md:px-7 xl:px-10">
+            <div className="mb-8 rounded-[20px] bg-white p-7 shadow-md hover:shadow-lg md:px-7 xl:px-10">
               <div className="mb-8 flex h-[70px] w-[70px] items-center justify-center rounded-2xl bg-primary">
                 <svg
                   width="39"
@@ -61,17 +61,18 @@ export const ServiceList: React.FC = () => {
                   />
                 </svg>
               </div>
-              <h4 className="text-dark mb-3 text-xl font-semibold">
-                Based on Tailwind CSS
+              <h4 className="mb-3 text-xl font-semibold text-black">
+                Calorie Counter
               </h4>
-              <p className="text-body-color">
-                We dejoy working with discerning clients, people for whom
-                qualuty, service, integrity & aesthetics.
+              <p className="text-black">
+                Although calories are only one aspect of your health and
+                wellness journey, it’s one way to quantify your efforts. Count
+                the number of calories burned from any exercise.
               </p>
             </div>
           </div>
           <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-            <div className="mb-8 rounded-[20px] bg-white p-10 shadow-md hover:shadow-lg md:px-7 xl:px-10">
+            <div className="mb-8 rounded-[20px] bg-white p-7 shadow-md hover:shadow-lg md:px-7 xl:px-10">
               <div className="mb-8 flex h-[70px] w-[70px] items-center justify-center rounded-2xl bg-primary">
                 <svg
                   width="35"
@@ -98,16 +99,15 @@ export const ServiceList: React.FC = () => {
                   />
                 </svg>
               </div>
-              <h4 className="text-dark mb-3 text-xl font-semibold">
-                100+ Components
+              <h4 className="mb-3 text-xl font-semibold text-black">
+                Reminders
               </h4>
-              <p className="text-body-color">
-                We dejoy working with discerning clients, people for whom
-                qualuty, service, integrity & aesthetics.
+              <p className="text-black">
+                Build a steady fitness schedule by creating reminders.
               </p>
             </div>
           </div>
-          <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+          {/* <div className="w-full px-4 md:w-1/2 lg:w-1/3">
             <div className="mb-8 rounded-[20px] bg-white p-10 shadow-md hover:shadow-lg md:px-7 xl:px-10">
               <div className="mb-8 flex h-[70px] w-[70px] items-center justify-center rounded-2xl bg-primary">
                 <svg
@@ -197,7 +197,7 @@ export const ServiceList: React.FC = () => {
                 qualuty, service, integrity & aesthetics.
               </p>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
