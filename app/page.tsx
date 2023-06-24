7c6999094f3fbe4318b112c8b8199c7c395a6a00
Paryssatis Khazaie <paryssatis@outlook.com>
2023-06-23 17:21:51 -0700

    #38 Rough version of service section for home page added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { MarketingPage } from "@templates/MarketingPage";
 import { MarketingHero } from "@organisms/MarketingHero";
+import { ServiceList } from "@organisms/ServiceList";
 
 export default function Home() {
   return (
     <MarketingPage>
       <MarketingHero />
+      <ServiceList />
     </MarketingPage>
   );
 }
