0dcd7d3a01a8f8a2fb52732d37feef1dbd7eaef2
Spencer W <4006420+seidior@users.noreply.github.com>
2023-06-26 15:42:20 -0700

    #38 Separate out ServicesIcon element

---
+++ stories/components/atoms/ServicesIcon/index.tsx
@@ -0,0 +1,7 @@
+export const ServicesIcon: React.FC<React.PropsWithChildren> = ({
+  children
+}) => (
+  <div className="mb-8 flex h-16 w-16 items-center justify-center rounded-2xl bg-ctaColor-light text-white dark:bg-ctaColor-dark dark:text-black">
+    {children}
+  </div>
+);
