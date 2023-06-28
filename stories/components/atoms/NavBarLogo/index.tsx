9d9cee32850eef7464216857f88a38fc3acad4a6
Paryssatis Khazaie <paryssatis@outlook.com>
2023-06-27 21:15:25 -0700

    #43 Implemented Navbar With/Without user option

--- stories/components/atoms/NavBarLogo/index.tsx
+++ stories/components/atoms/NavBarLogo/index.tsx
@@ -10,7 +10,7 @@ export const NavBarLogo: React.FC = () => {
   };
 
   return (
-    <div className="w-full flex-1 font-bold md:w-auto md:flex-none">
+    <div className="w-1/4 justify-start font-bold lg:w-1/3">
       <button type="button" onClick={clickHandler} className="w-24 p-1">
         <div
           className="h-10 w-24 bg-transparent bg-[url('/images/wellpath-logo-color.svg')] bg-no-repeat [mask-position:center] [mask-repeat:no-repeat] focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-slate-500 dark:bg-white dark:bg-none dark:[mask-image:url('/images/wellpath-logo-mono.svg')]"
