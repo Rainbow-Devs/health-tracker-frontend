dcf8b2b9936ecf7a777459941caaecd717ca82a3
Spencer W <4006420+seidior@users.noreply.github.com>
2023-07-02 19:30:04 -0700

    #39 Profile page for next step in auth process

---
+++ app/user/profile/page.tsx
@@ -0,0 +1,9 @@
+export const ProfilePage = () => (
+  <main className="flex flex-col justify-between">
+    <div className="container min-h-screen max-w-screen-lg p-4">
+      <p>This will be the profile page where users can set their goals.</p>
+    </div>
+  </main>
+);
+
+export default ProfilePage;
